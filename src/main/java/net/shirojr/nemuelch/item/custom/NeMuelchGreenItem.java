e312d97c83d17ee100ab2264c90c4c9ba91432c9
JR1811 <janreichelt77@gmail.com>
2022-06-14 06:00:44 +0200

    added tooltip lore and explanation to the items and translated them (en/de)

--- src/main/java/net/shirojr/nemuelch/item/custom/NeMuelchGreenItem.java
+++ src/main/java/net/shirojr/nemuelch/item/custom/NeMuelchGreenItem.java
@@ -1,6 +1,8 @@
 package net.shirojr.nemuelch.item.custom;
 
 import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -8,7 +10,12 @@ import net.minecraft.item.Items;
 import net.minecraft.item.MilkBucketItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class NeMuelchGreenItem extends MilkBucketItem {
 
@@ -33,4 +40,18 @@ public class NeMuelchGreenItem extends MilkBucketItem {
         return stack.isEmpty() ? new ItemStack(Items.BUCKET) : stack;
     }
 
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(new TranslatableText("item.nemuelch.green_muelch.tooltip.shift.line1"));
+            tooltip.add(new TranslatableText("item.nemuelch.green_muelch.tooltip.shift.line2"));
+            tooltip.add(new TranslatableText("item.nemuelch.green_muelch.tooltip.shift.line3"));
+            tooltip.add(new TranslatableText("item.nemuelch.green_muelch.tooltip.shift.line4"));
+        }
+
+        else {
+            tooltip.add(new TranslatableText("item.nemuelch.tooltip.expand.line1"));
+            tooltip.add(new TranslatableText("item.nemuelch.tooltip.expand.line2"));
+        }
+    }
 }
