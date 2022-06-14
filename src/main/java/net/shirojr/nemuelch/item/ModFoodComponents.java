b7800afb707bfffa10abe332f921221c6809ba39
JR1811 <janreichelt77@gmail.com>
2022-06-14 02:44:26 +0200

    Fixed consume sound, added clearing of all previous effects and added new effects for the special consumables #3

--- src/main/java/net/shirojr/nemuelch/item/ModFoodComponents.java
+++ src/main/java/net/shirojr/nemuelch/item/ModFoodComponents.java
@@ -1,18 +1,16 @@
 package net.shirojr.nemuelch.item;
 
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.MilkBucketItem;
 
 public class ModFoodComponents {
 
+    //TODO: Doesn't restore hunger and saturation back anymore since StatusEffectInstances have been moved to their respective custom item classes.
+
     public static final FoodComponent GREEN_MILK = new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).alwaysEdible().build();
-    public static final FoodComponent BLUE_MILK = new FoodComponent.Builder().hunger(8).saturationModifier(1.2F).statusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 2400, 0), 1.0F).alwaysEdible().build();
+    public static final FoodComponent BLUE_MILK = new FoodComponent.Builder().hunger(8).saturationModifier(1.2F).alwaysEdible().build();
     public static final FoodComponent BROWN_MILK = new FoodComponent.Builder().hunger(8).saturationModifier(1.2F).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 500, 2), 1.0F).alwaysEdible().build();
     public static final FoodComponent PINK_MILK = new FoodComponent.Builder().hunger(8).saturationModifier(1.2F).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 700, 0), 1.0F).alwaysEdible().build();
     public static final FoodComponent YELLOW_MILK = new FoodComponent.Builder().hunger(8).saturationModifier(1.2F).statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 100, 3), 1.0F).alwaysEdible().build();
-
 }
--- src/main/java/net/shirojr/nemuelch/item/ModItems.java
+++ src/main/java/net/shirojr/nemuelch/item/ModItems.java
@@ -6,25 +6,26 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.shirojr.nemuelch.NeMuelch;
+import net.shirojr.nemuelch.item.custom.*;
 
 public class ModItems {
 
     //new items
 
     public static final Item GREEN_MUELCH = registerItem("green_muelch",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.GREEN_MILK)));
+            new NeMuelchGreenItem(new FabricItemSettings().group(ItemGroup.MISC).food(ModFoodComponents.GREEN_MILK)));
 
     public static final Item BROWN_MUELCH = registerItem("brown_muelch",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.BROWN_MILK)));
+            new NeMuelchBrownItem(new FabricItemSettings().group(ItemGroup.MISC).food(ModFoodComponents.BROWN_MILK)));
 
     public static final Item BLUE_MUELCH = registerItem("blue_muelch",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.BLUE_MILK)));
+            new NeMuelchBlueItem(new FabricItemSettings().group(ItemGroup.MISC).food(ModFoodComponents.BLUE_MILK)));
 
     public static final Item PINK_MUELCH = registerItem("pink_muelch",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.PINK_MILK)));
+            new NeMuelchPinkItem(new FabricItemSettings().group(ItemGroup.MISC).food(ModFoodComponents.PINK_MILK)));
 
     public static final Item YELLOW_MUELCH = registerItem("yellow_muelch",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.YELLOW_MILK)));
+            new NeMuelchYellowItem(new FabricItemSettings().group(ItemGroup.MISC).food(ModFoodComponents.YELLOW_MILK)));
 
 
 
