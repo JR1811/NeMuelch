0a1c919a59ff8aebd145e034734ecf482ab2e238
JR1811 <janreichelt77@gmail.com>
2023-09-21 02:38:21 +0200

    added OminousHeartItem tick reset

--- src/main/java/net/shirojr/nemuelch/item/custom/supportItem/OminousHeartItem.java
+++ src/main/java/net/shirojr/nemuelch/item/custom/supportItem/OminousHeartItem.java
@@ -1,23 +1,17 @@
 package net.shirojr.nemuelch.item.custom.supportItem;
 
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.fabricmc.fabric.api.server.PlayerStream;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import net.shirojr.nemuelch.NeMuelch;
 import net.shirojr.nemuelch.init.ConfigInit;
-import net.shirojr.nemuelch.sound.NeMuelchSounds;
-
-import java.util.stream.Stream;
 
 public class OminousHeartItem extends Item {
 
@@ -32,18 +26,15 @@ public class OminousHeartItem extends Item {
         if (world.isClient()) return;
 
         if (tickCount % 60 == 0) {
-
             PacketByteBuf buf = PacketByteBufs.create();
             buf.writeBlockPos(entity.getBlockPos());
 
             for (ServerPlayerEntity player : PlayerLookup.around((ServerWorld) world, entity.getPos(), ConfigInit.CONFIG.ominousHeartBeatRange)) {
                 ServerPlayNetworking.send(player, NeMuelch.SOUND_PACKET_ID, buf);
             }
-            /*entity.playSound(NeMuelchSounds.ITEM_OMINOUS_HEART,
-                    ConfigInit.CONFIG.ominousHeartVolume,
-                    ConfigInit.CONFIG.ominousHeartPitch);*/
+            tickCount = 0;
         }
 
-        tickCount ++;
+        tickCount++;
     }
 }
