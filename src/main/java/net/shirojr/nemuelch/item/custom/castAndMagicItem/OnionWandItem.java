b008b048603fd1e2f48c788f9ce2aac14b1bdd24
JR1811 <janreichelt77@gmail.com>
2024-11-02 06:17:58 +0100

    update and small refactor

--- src/main/java/net/shirojr/nemuelch/item/custom/castAndMagicItem/OnionWandItem.java
+++ src/main/java/net/shirojr/nemuelch/item/custom/castAndMagicItem/OnionWandItem.java
@@ -1,17 +1,15 @@
 package net.shirojr.nemuelch.item.custom.castAndMagicItem;
 
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.shirojr.nemuelch.entity.NeMuelchEntities;
-import net.shirojr.nemuelch.entity.custom.OnionEntity;
 import net.shirojr.nemuelch.init.ConfigInit;
 import net.shirojr.nemuelch.item.materials.OnionWandMaterial;
 
@@ -22,24 +20,33 @@ public class OnionWandItem extends ToolItem {
         super(OnionWandMaterial.INSTANCE, settings);
     }
 
-    // usage cool down of the item
     private static final int USE_COOLDOWN_TICKS = 600;
-    // count of entities summoned by one use
-    private static final int MIN_COUNT = ConfigInit.CONFIG.onion.getToolData().getMinAmountSpawn();
-    private static final int MAX_COUNT = ConfigInit.CONFIG.onion.getToolData().getMaxAmountSpawn() + 1; // bound is exclusive
+    private static final int MIN_SPAWN_COUNT = ConfigInit.CONFIG.onion.getToolData().getMinAmountSpawn();
+    private static final int MAX_SPAWN_COUNT = ConfigInit.CONFIG.onion.getToolData().getMaxAmountSpawn() + 1;
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         Random random = world.random;
 
-        if (!world.isClient()) {
-            for (int i = 0; i < world.random.nextInt(MIN_COUNT, MAX_COUNT); i++) {
-
-                OnionEntity onion = new OnionEntity(world, user.getUuid());
-                onion.setPos(user.getX() + random.nextDouble(-5,5), user.getY() + 1, user.getZ() + random.nextDouble(-5, 5));
-
-                world.spawnEntity(onion);
+        if (world instanceof ServerWorld serverWorld) {
+            for (int i = 0; i < world.random.nextInt(MIN_SPAWN_COUNT, MAX_SPAWN_COUNT); i++) {
+                int spawnAttempts = 10;
+                BlockPos.Mutable pos = new BlockPos.Mutable(
+                        user.getX() + random.nextDouble(-5, 5),
+                        user.getY() + 1,
+                        user.getZ() + random.nextDouble(-5, 5)
+                );
+                while (!serverWorld.getBlockState(pos).isAir() && spawnAttempts >= 0) {
+                    pos.set(
+                            user.getX() + random.nextDouble(-5, 5),
+                            user.getY() + 1,
+                            user.getZ() + random.nextDouble(-5, 5)
+                    );
+                    spawnAttempts--;
+                }
+                if (spawnAttempts <= 0) continue;
+                NeMuelchEntities.ONION.spawn(serverWorld, null, null, user, pos, SpawnReason.BREEDING, false, false);
             }
 
             user.getItemCooldownManager().set(this, USE_COOLDOWN_TICKS);
