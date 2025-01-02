5b56eb6378d696f14854cc46d024027686174645
JR1811 <janreichelt77@gmail.com>
2025-01-02 22:53:40 +0100

    removed step sounds for illusions

--- src/main/java/net/shirojr/nemuelch/mixin/EntityIllusionMixin.java
+++ src/main/java/net/shirojr/nemuelch/mixin/EntityIllusionMixin.java
@@ -4,11 +4,14 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.Entity;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 import net.shirojr.nemuelch.util.Illusionable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityIllusionMixin {
@@ -20,17 +23,23 @@ public class EntityIllusionMixin {
                                                     double x, double y, double z,
                                                     double velocityX, double velocityY, double velocityZ,
                                                     Operation<Void> original) {
-        if (!world.isClient()) {
-            original.call(instance, parameters, x, y, z, velocityX, velocityY, velocityZ);
-            return;
-        }
-        // MinecraftClient client = MinecraftClient.getInstance();
         Entity entity = (Entity) (Object) this;
         boolean isIllusion = entity instanceof Illusionable restrictedRendering && restrictedRendering.nemuelch$isIllusion();
-        // boolean visibleParticles = /*client.player != null &&*/ NeMuelchClient.ILLUSIONS_CACHE.contains(entity);
-
-        if (!isIllusion /*|| visibleParticles*/) {
+        if (!isIllusion) {
             original.call(instance, parameters, x, y, z, velocityX, velocityY, velocityZ);
         }
+    }
+
+    @WrapOperation(method = "playStepSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;playSound(Lnet/minecraft/sound/SoundEvent;FF)V"))
+    private void avoidStepSound(Entity instance, SoundEvent sound, float volume, float pitch, Operation<Void> original) {
+        if (!(instance instanceof Illusionable illusion) || !illusion.nemuelch$isIllusion()) {
+            original.call(instance, sound, volume, pitch);
+        }
+    }
+
+    @Inject(method = "isSilent", at = @At("HEAD"), cancellable = true)
+    private void avoidIllusionSound(CallbackInfoReturnable<Boolean> cir) {
+        if (!((Entity) (Object) this instanceof Illusionable illusion)) return;
+        if (illusion.nemuelch$isIllusion()) cir.setReturnValue(true);
     }
 }
