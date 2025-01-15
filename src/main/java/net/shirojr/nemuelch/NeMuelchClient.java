259608490327451393f8393734ab291f39e55f87
JR1811 <janreichelt77@gmail.com>
2025-01-15 16:40:36 +0100

    initial clean setup with dependencies

---
+++ src/main/java/net/shirojr/nemuelch/NeMuelchClient.java
@@ -0,0 +1,10 @@
+package net.shirojr.nemuelch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeMuelchClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
