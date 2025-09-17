0eab0f4182af9283bd2b75b478895e8166a6df12
JR1811 <janreichelt77@gmail.com>
2025-09-18 01:43:27 +0200

    turned down default act max range to 20

--- src/main/java/net/shirojr/nemuelch/config/NeMuelchConfig.java
+++ src/main/java/net/shirojr/nemuelch/config/NeMuelchConfig.java
@@ -52,7 +52,7 @@ public class NeMuelchConfig implements ConfigData {
     @Comment("For Whitelist Entries, check nemuelch Block Tags")
     public boolean enableFertilizableBlockWhitelistFeature = true;
     public boolean enableRandomTickChanceLimitFeature = true;
-    public double actCommandMaxRange = 32;
+    public double actCommandMaxRange = 20;
     public boolean printActCommandInChat = true;
     public boolean printActCommandInActionBar = true;
 }
