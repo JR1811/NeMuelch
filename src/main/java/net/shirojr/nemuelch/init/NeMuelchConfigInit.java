25084ab2facaeb2b51eb2c545e59e653e25bf0ac
JR1811 <janreichelt77@gmail.com>
2025-10-01 01:03:51 +0200

    fixed config value resetting bug by changing config file format to .toml

--- src/main/java/net/shirojr/nemuelch/init/NeMuelchConfigInit.java
+++ src/main/java/net/shirojr/nemuelch/init/NeMuelchConfigInit.java
@@ -1,14 +1,14 @@
 package net.shirojr.nemuelch.init;
 
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.shirojr.nemuelch.config.NeMuelchConfig;
 
 public class NeMuelchConfigInit {
     public static NeMuelchConfig CONFIG = new NeMuelchConfig();
 
     public static void initialize() {
-        AutoConfig.register(NeMuelchConfig.class, JanksonConfigSerializer::new);
+        AutoConfig.register(NeMuelchConfig.class, Toml4jConfigSerializer::new);
         CONFIG = AutoConfig.getConfigHolder(NeMuelchConfig.class).getConfig();
     }
 }
