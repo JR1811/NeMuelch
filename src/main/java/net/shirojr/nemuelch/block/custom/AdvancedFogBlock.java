d4973ddefd1bd9d6d7c7ce9fcf6a5a2226ac2b50
JR1811 <janreichelt77@gmail.com>
2025-12-20 03:07:57 +0100

    removed unused ticking from AdvancedFogBlock

--- src/main/java/net/shirojr/nemuelch/block/custom/AdvancedFogBlock.java
+++ src/main/java/net/shirojr/nemuelch/block/custom/AdvancedFogBlock.java
@@ -5,16 +5,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityTicker;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import net.shirojr.nemuelch.block.entity.custom.AdvancedFogBlockEntity;
 import net.shirojr.nemuelch.block.util.HittableWithItem;
-import net.shirojr.nemuelch.init.NeMuelchBlockEntities;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("deprecation")
@@ -43,8 +39,8 @@ public class AdvancedFogBlock extends BlockWithEntity implements HittableWithIte
         return BlockRenderType.ENTITYBLOCK_ANIMATED;
     }
 
-    @Override
+    /*@Override
     public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         return checkType(type, NeMuelchBlockEntities.ADVANCED_FOG, AdvancedFogBlockEntity::tick);
-    }
+    }*/
 }
--- src/main/java/net/shirojr/nemuelch/block/entity/custom/AdvancedFogBlockEntity.java
+++ src/main/java/net/shirojr/nemuelch/block/entity/custom/AdvancedFogBlockEntity.java
@@ -15,7 +15,6 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.world.World;
 import net.shirojr.nemuelch.init.NeMuelchBlockEntities;
 import net.shirojr.nemuelch.network.util.MiscClientSideCalls;
 import net.shirojr.nemuelch.network.util.NetworkIdentifiers;
@@ -76,10 +75,6 @@ public class AdvancedFogBlockEntity extends BlockEntity {
             }
         }
         super.markDirty();
-    }
-
-    public static void tick(World world, BlockPos pos, BlockState state, AdvancedFogBlockEntity blockEntity) {
-
     }
 
     @Override
