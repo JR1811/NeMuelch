8da24a35abf97c20e3455d3b800a9cbbc240422a
JR1811 <janreichelt77@gmail.com>
2026-01-13 04:43:40 +0100

    added new cryptic sound

--- src/main/java/net/shirojr/nemuelch/init/NeMuelchSounds.java
+++ src/main/java/net/shirojr/nemuelch/init/NeMuelchSounds.java
@@ -64,6 +64,7 @@ public interface NeMuelchSounds {
 
     SoundEvent CRYPTIC_POEM_01 = register("cryptic_poem_dead_captain");
     SoundEvent CRYPTIC_CHANT_01 = register("cryptic_chant_01");
+    SoundEvent CRYPTIC_CHANT_02 = register("cryptic_chant_02");
 
 
     static SoundEvent register(String id) {
