e0d69d546372bfd29a16467c2602f16abe039705
JR1811 <janreichelt77@gmail.com>
2026-03-11 10:42:58 +0100

    changed PullUpFeatureHelper conditions

--- src/main/java/net/shirojr/nemuelch/util/helper/PullUpFeatureHelper.java
+++ src/main/java/net/shirojr/nemuelch/util/helper/PullUpFeatureHelper.java
@@ -2,7 +2,9 @@ package net.shirojr.nemuelch.util.helper;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Saddleable;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -18,18 +20,22 @@ public class PullUpFeatureHelper {
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     public static boolean canPullUp(@Nullable PlayerEntity source, @Nullable Entity target) {
         if (source == null || !(target instanceof LivingEntity targetEntity)) return false;
-        if (source.isSneaking() || source.isSpectator()) return false;
+        if (source.isSneaking() || source.isSpectator() || target.isSpectator()) return false;
         if (!source.getMainHandStack().isEmpty()) return false;
+        if (!target.isInPose(EntityPose.STANDING)) return false;
         MiscEntityComponent component = MiscEntityComponent.get(source);
         if (component.isPullUpOnCooldown()) return false;
         if (!FabricLoader.getInstance().isDevelopmentEnvironment() && !source.isCreative()) {
             if (targetEntity.isOnGround() || targetEntity.fallDistance > 0) return false;
             if (targetEntity.hurtTime > 0) return false;
         }
+        if (source.isCreative() && !(target instanceof Saddleable)) {
+            return true;
+        }
         if (source.getEyeY() < targetEntity.getEyeY()) return false;
         double sourceSize = source.getBoundingBox().getAverageSideLength();
         double targetSize = target.getBoundingBox().getAverageSideLength();
-        return source.isCreative() || sourceSize >= targetSize;
+        return sourceSize >= targetSize;
     }
 
     public static void applyPullUp(PlayerEntity source, Entity targetEntity) {
