30080127434dc02dfe6702b2bef35b631c3f13e3
JR1811 <janreichelt77@gmail.com>
2026-06-05 16:06:02 +0200

    added JavaDoc explanation to MobPersistency

--- src/main/java/net/shirojr/nemuelch/util/duck/MobPersistency.java
+++ src/main/java/net/shirojr/nemuelch/util/duck/MobPersistency.java
@@ -1,5 +1,11 @@
 package net.shirojr.nemuelch.util.duck;
 
+import net.minecraft.entity.mob.MobEntity;
+
+/**
+ * Default persistency methods don't allow for full control
+ * @see MobEntity#setPersistent()
+ */
 public interface MobPersistency {
     boolean nemuelch$isPersistent();
 
