334f7d8b7b7783c2dfac1ee2ac24c0d96bf09f0d
JR1811 <janreichelt77@gmail.com>
2026-06-12 17:37:39 +0200

    potentially fixed BlockFinderRenderer delay by using a custom BufferBuilder

--- src/main/java/net/shirojr/nemuelch/render/BlockFinderRenderer.java
+++ src/main/java/net/shirojr/nemuelch/render/BlockFinderRenderer.java
@@ -1,5 +1,6 @@
 package net.shirojr.nemuelch.render;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
@@ -13,35 +14,26 @@ import net.minecraft.util.math.Vec3d;
 
 import java.util.HashSet;
 
-public class BlockFinderRenderer implements WorldRenderEvents.Last {
+public class BlockFinderRenderer implements WorldRenderEvents.AfterTranslucent {
     public static final HashSet<BlockPos> DISPLAYED_POS = new HashSet<>();
 
     @Override
-    public void onLast(WorldRenderContext context) {
+    public void afterTranslucent(WorldRenderContext context) {
         if (DISPLAYED_POS.isEmpty()) return;
         MinecraftClient client = MinecraftClient.getInstance();
         ClientPlayerEntity player = client.player;
         if (player == null) return;
-        MatrixStack matrixStack = context.matrixStack();
-        VertexConsumerProvider consumers = context.consumers();
-        if (consumers == null) return;
         float normalizedAlphaPulse = (MathHelper.sin((player.age + context.tickDelta()) * 0.1f) + 1f) * 0.5f;
 
-        RenderLayer NO_DEPTH = RenderLayer.of(
-                "block_finder",
-                VertexFormats.POSITION_COLOR,
-                VertexFormat.DrawMode.TRIANGLE_STRIP,
-                256,
-                RenderLayer.MultiPhaseParameters.builder()
-                        .program(RenderPhase.COLOR_PROGRAM)
-                        .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
-                        .transparency(RenderPhase.TRANSLUCENT_TRANSPARENCY)
-                        .target(RenderPhase.MAIN_TARGET)
-                        .writeMaskState(RenderPhase.COLOR_MASK)
-                        .depthTest(RenderPhase.ALWAYS_DEPTH_TEST)
-                        .cull(RenderPhase.DISABLE_CULLING)
-                        .build(false)
-        );
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableDepthTest();
+
+        BufferBuilder bufferBuilder = new BufferBuilder(400 * DISPLAYED_POS.size());
+        bufferBuilder.begin(VertexFormat.DrawMode.TRIANGLE_STRIP, VertexFormats.POSITION_COLOR);
+
+        MatrixStack matrixStack = context.matrixStack();
 
         matrixStack.push();
         Vec3d cam = context.camera().getPos();
@@ -49,10 +41,15 @@ public class BlockFinderRenderer implements WorldRenderEvents.Last {
 
         for (BlockPos pos : DISPLAYED_POS) {
             Box box = new Box(pos);
-            WorldRenderer.renderFilledBox(matrixStack, consumers.getBuffer(NO_DEPTH),
+            WorldRenderer.renderFilledBox(matrixStack, bufferBuilder,
                     box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ,
                     0.2f, 0.6f, 0.3f, MathHelper.lerp(normalizedAlphaPulse, 0f, 0.6f));
         }
         matrixStack.pop();
+
+        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+
+        RenderSystem.enableDepthTest();
+        RenderSystem.disableBlend();
     }
 }
