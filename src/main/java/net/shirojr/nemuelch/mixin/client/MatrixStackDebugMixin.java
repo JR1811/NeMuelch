23aeb593bc31e5f87823034e7bae09b952e1d643
JR1811 <janreichelt77@gmail.com>
2026-07-26 00:23:40 +0200

    small logger cleanup

--- src/main/java/net/shirojr/nemuelch/mixin/client/MatrixStackDebugMixin.java
+++ src/main/java/net/shirojr/nemuelch/mixin/client/MatrixStackDebugMixin.java
@@ -1,7 +1,9 @@
 package net.shirojr.nemuelch.mixin.client;
 
 import net.minecraft.client.util.math.MatrixStack;
-import org.apache.logging.log4j.LogManager;
+import net.shirojr.nemuelch.NeMuelch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,6 +15,10 @@ import java.util.ArrayDeque;
 
 @Mixin(MatrixStack.class)
 public class MatrixStackDebugMixin {
+    @Unique
+    private static final Logger DEBUG_LOGGER = LoggerFactory.getLogger(NeMuelch.MOD_ID + " (MatrixStackDebug Helper)");
+
+
     @Unique
     private final ArrayDeque<Throwable> pushSites = new ArrayDeque<>();
 
@@ -29,9 +35,7 @@ public class MatrixStackDebugMixin {
     @Inject(method = "isEmpty", at = @At("RETURN"))
     private void onIsEmpty(CallbackInfoReturnable<Boolean> cir) {
         if (!cir.getReturnValue() && !pushSites.isEmpty()) {
-            LogManager.getLogger("MatrixStackDebug").error(
-                    "=== LEAKED PUSH SITE ===", pushSites.peek()
-            );
+            DEBUG_LOGGER.error("=== LEAKED PUSH SITE ===", pushSites.peek());
         }
     }
 }
